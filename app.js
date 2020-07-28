b972d80424c166c14d8349b9b450bdd4cfb0fa4a
ShihabShahib <shahibshihab@gmail.com>
2020-07-28 23:42:23 +0600

    T2M1F1

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 var login = require('./controller/login');
+var admin = require('./controller/admin');
+var employee = require('./controller/employee');
+
 
 app.set('view engine', 'ejs');
 
@@ -9,8 +12,8 @@ app.set('view engine', 'ejs');
 //middleware
 app.use(bodyParser());
 app.use('/login', login);
-
-
+app.use('/admin', admin);
+app.use('/employee', employee);
 
 app.listen(3000, function(){
     console.log('express http server started at 3000....')
--- controller/login.js
+++ controller/login.js
@@ -6,7 +6,15 @@ router.get('/',function(req,res){
 });
 
 router.post('/',function(req,res){
-    res.render('login');
+    if(req.body.uname == "admin" && req.body.password == "admin"){
+        res.redirect('/admin');
+    }
+    else if(req.body.uname == "employee" && req.body.password == "employee"){
+        res.redirect('/employee');
+    }
+    else{
+        res.send('Invalid username or password');
+    }
 });
 
 module.exports = router;
