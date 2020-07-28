187fa9361bc6a52f0c008c0dd4029e081dd649d8
ShihabShahib <shahibshihab@gmail.com>
2020-07-29 02:42:59 +0600

    T2M2F2

--- controller/admin.js
+++ controller/admin.js
@@ -5,4 +5,17 @@ router.get('/',function(req,res){
     res.render('admin');
 });
 
+router.post('/', function(req, res){
+    if(req.body.AddEmployee == "AddEmployee"){
+        res.redirect('/admin/addemployee');
+    }else if(req.body.AllEmployeelist == "AllEmployeelist"){
+        res.redirect('/admin/employeelist');
+    }else{
+        res.render('admin');
+    }
+});
+router.get('/addemployee',function(req,res){
+    res.render('addemployee');
+});
+
 module.exports=router;
--- controller/login.js
+++ controller/login.js
@@ -7,7 +7,7 @@ router.get('/',function(req,res){
 
 router.post('/',function(req,res){
     if(req.body.uname == "admin" && req.body.password == "admin"){
-        res.redirect('/admin');
+        res.redirect('/admin/');
     }
     else if(req.body.uname == "employee" && req.body.password == "employee"){
         res.redirect('/employee');
